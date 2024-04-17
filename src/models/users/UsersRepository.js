a795d7ebc51fa1f522c9fce61ec1794b16e087be
JuliaFee <julia.silva133@aluno.senai.br>
2024-04-17 08:19:05 -0300

    kdkdkd

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -8,33 +8,47 @@ export default class UsersRepository {
   }
 
   async getUsers() {
-    const allUsers = await this.db.manyOrNone("SELECT * FROM users");
-
-    // console.log(allUsers);
-    return allUsers;
+    try{
+      const allUsers = await this.db.manyOrNone("SELECT * FROM users");
+      return allUsers;
+     } catch (error){
+      console.error("cocozao", error);
+      throw error;
+     }
   }
 
 async getUserById(id) {
-  const user = await this.db.oneOrNone("SELECT * FROM users WHERE id = $1", id);
-    // console.log("user", user);
-    return user;
+  try{
+    const user = await this.db.oneOrNone("SELECT * FROM users WHERE id = $1", id);
+    return user
+    } catch (error){
+      console.error(`cocozao do id ${id}`, error);
+      throw error;
+    }
   }
 
   async getUserByEmail(email) {
-    const user = await this.db.oneOrNone(
-      "SELECT * FROM users WHERE email = $1",
-      email
-    );
-    console.log();
-    return user;
+    try{
+      const user = await this.db.oneOrNone("SELECT * FROM users WHERE email = $1", email);
+      return user
+      } catch (error){
+        console.error(`cocozao do email ${email}`, error);
+        throw error;
+      }
   }
 
   async createUser(user) {
-    await this.db.none("INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)", [user.id, user.name, user.email, user.password]);
+    try {
+      await this.db.none("INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)", [user.id, user.name, user.email, user.password]);
     return user;
+    } catch (error) {
+      console.error("cocozao nao criou usuario: ", error);
+      throw error
+    }
   }
 
   async updateUser(id, name, email, password) {
+   try {
     const user = await this.getUserById(id);
 
     if (!user) {
@@ -50,9 +64,18 @@ async getUserById(id) {
     )
 
     return user;
+   } catch (error) {
+  console.error(`cocozao update do id ${id}`, error);
+  throw error
+   }
   }
 
-  deleteUser(id) {
-    this.users = this.users.filter((user) => user.id !== id);
+  async deleteUser(id) {
+   try {
+    await this.db.none("DELETE FROM users WHERE id = $1", id);
+   } catch (error) {
+    console.error(`cocozao delete do id ${id}`, error);
+    throw error
+   }
   }
 }
